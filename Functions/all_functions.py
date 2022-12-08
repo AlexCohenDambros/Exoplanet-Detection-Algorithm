6bf7338540c5ecb9453a7cc91db013ef4321853c
Alex Cohen <alex-dambros@hotmail.com>
2022-12-08 00:20:59 -0300

    added test for data download

--- Functions/all_functions.py
+++ Functions/all_functions.py
@@ -1,5 +1,5 @@
 # coding = utf-8
-# Author: Alex Cohen Dambrós Lopes 
+# Author: Alex Cohen Dambrós Lopes
 
 """
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -16,6 +16,7 @@ K2: https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=Ex
 
 
 # ============= Imports =============
+from io import StringIO
 import pandas as pd
 import numpy as np
 import lightkurve as lk
@@ -36,59 +37,71 @@ warnings.simplefilter("ignore")
 
 
 # ============= Read Datasets =============
-def read_dataset(telescope_name = None, header_line = 0):
-    
+def read_dataset(telescope_name=None):
+
     telescopes = ['tess', 'k2', 'kepler']
-    
-    if telescope_name == None: 
-        raise TypeError("read_dataset() missing 1 required positional argument: 'telescope_name'")
-    
+
+    if telescope_name == None:
+        raise TypeError(
+            "read_dataset() missing 1 required positional argument: 'telescope_name'")
+
     elif telescope_name in telescopes:
-        
-        if telescope_name == 'tess': 
+
+        if telescope_name == 'tess':
             telescope_path = 'Datasets\TESS'
-            
+
         elif telescope_name == 'k2':
             telescope_path = 'Datasets\K2'
-        
+
         elif telescope_name == 'kepler':
             telescope_path = 'Datasets\KEPLER'
-            
+
     else:
-        raise ValueError(f"The argument telescope_name does not have the telescope data: '{telescope_name}'")
-    
+        raise ValueError(
+            f"The argument telescope_name does not have the telescope data: '{telescope_name}'")
+
     telescope_path = glob.glob(telescope_path+'\*.csv')
-    dataset = pd.read_csv(telescope_path[0], header = header_line, on_bad_lines='skip')
-    
+
+    csv_data = ''
+    with open(telescope_path[0], 'r') as f:
+        csv_data = '\n'.join(
+            list(filter(lambda a: not a.startswith('#'), f.readlines())))[1:]
+
+    dataset = pd.read_csv(StringIO(csv_data), on_bad_lines='skip')
 
     if telescope_name == 'tess':
-        names_disp = {"FP" : 'FALSE POSITIVE', "PC" : 'CANDIDATE', "CP": 'CONFIRMED', "FA": 'FALSE POSITIVE', "KP": 'CONFIRMED'}
+        names_disp = {"FP": 'FALSE POSITIVE', "PC": 'CANDIDATE',
+                      "CP": 'CONFIRMED', "FA": 'FALSE POSITIVE', "KP": 'CONFIRMED'}
         dataset.replace({'tfopwg_disp': names_disp}, inplace=True)
 
     return dataset
 
 
-def download_all_data():
-
+def download_all_datasets():
     """
         =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
         Bot created using web spring to automatically download data from NASA
         =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        
+
         This algorithm automatically selects and downloads data from the K2, KEPLER, and TESS space 
         telescopes from the NASA Exoplanet Archive website.
     """
-    
-    
+
     # ============= Options =============
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
 
+    # ============= Diretorio
+    prefs = {"profile.default_content_settings.popups": 0,
+             "download.default_directory": 
+                        r"C:/Users/alex-/Desktop/Dev/Exoplanet-Detection-Algorithm/Datasets/TESS",
+             "directory_upgrade": True}
+    chrome_options.add_experimental_option("prefs", prefs)
 
     # ============= Path =============
     PATH = "/path/to/chromedriver"
-    driver = webdriver.Chrome(PATH) # chrome_options = chrome_options  
-
+    # chrome_options = chrome_options
+    driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
 
     # ============= URL =============
     urlTESS = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=TOI'
@@ -97,13 +110,25 @@ def download_all_data():
 
     list_urls = [urlTESS, urlKEPLER, urlK2]
 
+    # ============= Passing through all URLs =============
+    # for url in list_urls:
 
-    # ============= Passing through all URLs ============= 
-    for url in list_urls:
+    #     try:
+    #         # ============= Open URL =============
+    #         driver.get(url)
+
+    #     except Exception as error:
+    #         print(error)
+    driver.get(urlTESS)
     
-        try:
-            # ============= Open URL =============
-            driver.get(url)
-
-        except Exception as error:
-            print(error)
+    time.sleep(5)
+    
+    driver.execute_script(
+        "document.baixar = () => [...document.querySelectorAll(`div`)].filter(a => a.textContent.includes(`Download Table`)).filter(a => a.className.includes(`sub_item_text`))[0].parentElement.click()")    
+    driver.execute_script("document.baixar()")
+    
+    time.sleep(15)
+    driver.close()
+    driver.quit()
+    
+download_all_datasets()
